93acfaf27509730dcbd9ae9b71b8fbe4b949063d
Nikita Vintonovich <nikitavintonovich@MacBook-Pro-Nikita.local>
2017-08-10 23:33:21 +0300

    Added example of variables

--- Lesson_2/Lesson_2/main.m
+++ Lesson_2/Lesson_2/main.m
@@ -12,6 +12,40 @@ int main(int argc, const char * argv[])
 {
     @autoreleasepool
     {
+        BOOL variableBool = YES;
+        variableBool = NO;
+        
+        int variableInt = 32;
+        variableInt = -45;
+        variableInt = 0;
+        
+        long variableLong = 1000000000;
+        variableLong = -1000000000;
+        
+        float variableFloat = 0.45;
+        variableFloat = -100.345;
+        
+        NSInteger variableInteger = 20;
+        variableInteger = -35;
+        variableInteger = 0;
+        
+        CGFloat variableCGFloat = 0.32f;
+        variableCGFloat = -35.00098f;
+        variableCGFloat = 99.25f;
+        
+        NSNumber *variableNumber = @(variableInt);
+        variableNumber = @(variableBool);
+        
+        char variableChar = 'c';
+        variableChar = '5';
+        
+        NSString *variableString = @"Some text";
+        variableString = @"Another text  ";
+        variableString = [NSString stringWithFormat:@"%@ %c", variableString, variableChar];
+        
+        NSArray *variableArray = @[@(variableBool), @(variableInt), @(variableLong), @(variableFloat), @(variableInteger), @(variableCGFloat), variableNumber, variableString];
+        
+        NSLog(@"Array contains elements: %lu", variableArray.count);
         
     }
     return 0;
