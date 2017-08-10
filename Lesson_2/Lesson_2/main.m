4aa9ae1073a3def1884840119a7990459d49288a
Nikita Vintonovich <nikitavintonovich@MacBook-Pro-Nikita.local>
2017-08-10 23:00:35 +0300

    Project was added

---
+++ Lesson_2/Lesson_2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lesson_2
+//
+//  Created by Nikita Vintonovich on 8/10/17.
+//  Copyright © 2017 Vint-Rock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        
+    }
+    return 0;
+}
