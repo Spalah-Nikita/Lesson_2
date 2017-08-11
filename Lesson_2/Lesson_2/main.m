d0ff03cdabfb439f7fb891bb9878ad2dc84b9621
Nikita Vintonovich <nikitavintonovich@MacBook-Pro-Nikita.local>
2017-08-12 01:48:19 +0300

    Посчитать и вывести в консоль, сколько раз встречается число 37 в массиве

--- Lesson_2/Lesson_2/main.m
+++ Lesson_2/Lesson_2/main.m
@@ -12,6 +12,48 @@ int main(int argc, const char * argv[])
 {
     @autoreleasepool
     {
+        // ЗАДАЧА:
+        // необходимо посчитать и вывести в консоль, сколько раз встречается число 37 в массиве
+        // если ни разу не встречается - вывести в консоль текст "Числа 37 не было найдено"
+        
+        
+        NSInteger itemsCount = 1000; // всего элементов в массиве
+        NSMutableArray *array = [NSMutableArray new]; // создание масива, элементы которого можно изменять
+        
+        for (NSInteger i = 0; i < itemsCount; i++)
+        {
+            [array addObject:@(arc4random() % 99)]; // добавляем случайное число в массив
+        }
+        
+        // таперь у нас есть массив array, содержащий 1000 элементов, каждый элемент - случайное число в диапазоне от 0 до 99
+        
+        
+        
+        // Пример работы программы:
+        
+        // в resultCount будем записывать количство найденных совпадений
+        NSInteger resultCount = 0;
+        
+        for (NSInteger i = 0; i < array.count; i++)
+        {
+            // достаем элемент массива используя переменную currentNumber
+            NSNumber *currentNumber = array[i];
+            
+            // сравниваем с числом 37. Если значение currentNumber равно 37 - увеличиваем счетчик resultCount
+            if ([currentNumber integerValue] == 37)
+            {
+                resultCount++;
+            }
+        }
+        
+        if (resultCount > 0)
+        {
+            NSLog(@"Число 37 встречается %lu раз", resultCount);
+        }
+        else
+        {
+            NSLog(@"Числа 37 не было найдено");
+        }
         
     }
     return 0;
